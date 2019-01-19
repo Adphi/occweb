5e7da4b5c7ca32fe7ecbe7d90ab4002bf2b6e76d
Adphi <philippe.adrien.nousse@gmail.com>
2019-01-19 16:38:43 +0100

    added alias c to clear and exit command

--- js/index.js
+++ js/index.js
@@ -8,20 +8,29 @@
     var baseUrl = OC.generateUrl('/apps/occweb');
     $.get(baseUrl + '/cmd', function(response){
       $('#app-content').terminal(function(command, term) {
-        var occCommand = {
-          command: command
-        };
-        term.pause();
-        $.ajax({
-          url: baseUrl + '/cmd',
-          type: 'POST',
-          contentType: 'application/json',
-          data: JSON.stringify(occCommand)
-        }).done(function (response) {
-          term.echo('\n'+ response).resume();
-        }).fail(function (response, code) {
-          term.echo('\n' + response).resume();
-        });
+        switch (command) {
+        case "c":
+          this.clear();
+          break;
+        case "exit":
+          this.reset();
+          break;
+        default:
+          var occCommand = {
+            command: command
+          };
+          term.pause();
+          $.ajax({
+            url: baseUrl + '/cmd',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(occCommand)
+          }).done(function (response) {
+            term.echo('\n' + response).resume();
+          }).fail(function (response, code) {
+            term.echo('\n' + response).resume();
+          });
+        }
       }, {
         greetings: '[[;green;]' + new Date().toString().slice(0, 24) + "]\n\nPress [[;#ff5e99;]Enter] for more information on [[;#009ae3;]occ] commands.\n",
         name: 'occ',
