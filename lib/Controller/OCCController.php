6856ac0eba9d76e211d2412814186c8d3c4d7a46
Adphi <philippe.adrien.nousse@gmail.com>
2019-01-19 16:32:50 +0100

    fix app compliance

--- lib/Controller/OCCController.php
+++ lib/Controller/OCCController.php
@@ -9,7 +9,6 @@ use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Controller;
 use OCP\ILogger;
 use Symfony\Component\Console\Input\StringInput;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class OCCController extends Controller implements IRequest
